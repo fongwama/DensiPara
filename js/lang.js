974c46fb9860b6f2f935f86b37e2afdbda5123b1
Pierre Poulain <pierre.poulain@cupnet.net>
2016-01-09 13:54:56 +0100

    Use html5 button

--- js/lang.js
+++ js/lang.js
@@ -1,29 +1,31 @@
 var translation = {
     "fr" : {
-        "label_nb_parasite"  : "Nombre de parasites",
-        "info_nb_parasite"   : "Veuillez saisir le nombre de parasites.",
-        "label_nb_wbc"       : "Nombre de globules blancs",
-        "info_nb_wbc"        : "Veuillez saisir le nombre de globules blancs.",
-        "label_nb_wbc_blood" : "Nombre de globules blancs / µL de sang",
-        "info_nb_wbc_blood"  : "Veuiller saisir le nombre de globules blancs / µL de sang.", 
-        "button_compute"     : "calculer",
-        "button_reset"       : "Réinitialiser",
-        "label_parasitemia"  : "Densité parasitaire",
-        "label_ref_id"       : "Numéro de référence",
-        "label_date_time"    : "Date et heure" 
+        "label_nb_parasite"       : "Nombre de parasites",
+        "info_nb_parasite"        : "Veuillez saisir le nombre de parasites.",
+        "label_nb_wbc"            : "Nombre de globules blancs",
+        "info_nb_wbc"             : "Veuillez saisir le nombre de globules blancs.",
+        "label_nb_wbc_blood"      : "Nombre de globules blancs / µL de sang",
+        "info_nb_wbc_blood"       : "Veuiller saisir le nombre de globules blancs / µL de sang.", 
+        "button_compute"          : "Calculer",
+        "button_reset"            : "Réinitialiser",
+        "label_parasitemia"       : "Densité parasitaire",
+        "label_parasitemia_unit"  : "parasites / µL de sang",
+        "label_ref_id"            : "Numéro de référence",
+        "label_date_time"         : "Date et heure" 
     },
     "en" : {
-        "label_nb_parasite"  : "Number of parasites",
-        "info_nb_parasite"   : "Please enter the number of parasites.",
-        "label_nb_wbc"       : "Number of white blood cells",
-        "info_nb_wbc"        : "Please enter the number of white blood cells.",
-        "label_nb_wbc_blood" : "Nombre of white blood cells / µL of blood",
-        "info_nb_wbc_blood"  : "Please enter the number of white blood cells / µL of blood.", 
-        "button_compute"     : "Compute",
-        "button_reset"       : "Reset",
-        "label_parasitemia"  : "Parasitemia",
-        "label_ref_id"       : "Reference id",
-        "label_date_time"    : "Date and time" 
+        "label_nb_parasite"       : "Number of parasites",
+        "info_nb_parasite"        : "Please enter the number of parasites.",
+        "label_nb_wbc"            : "Number of white blood cells",
+        "info_nb_wbc"             : "Please enter the number of white blood cells.",
+        "label_nb_wbc_blood"      : "Nombre of white blood cells / µL of blood",
+        "info_nb_wbc_blood"       : "Please enter the number of white blood cells / µL of blood.", 
+        "button_compute"          : "Compute",
+        "button_reset"            : "Reset",
+        "label_parasitemia"       : "Parasitemia",
+        "label_parasitemia_unit"  : "parasites / µL of blood",
+        "label_ref_id"            : "Reference id",
+        "label_date_time"         : "Date and time" 
     }
 }
 
