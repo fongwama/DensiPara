70bb923e9f8c18f449c35b45b445cb9c1668b10f
Pierre Poulain <pierre.poulain@cupnet.net>
2016-01-28 16:14:23 +0100

    Emphasis button names

--- js/translate.js
+++ js/translate.js
@@ -23,7 +23,7 @@ var translation = {
         "about_sponsors"          : "Ce projet est soutenu par nos partenaires : ",
         "about_license"           : "Cette application est un logiciel libre diffusé sous la licence ",
         "help_description"        : "La densité parasitaire est une estimation du nombre de parasites par microlitre de sang chez un individu infecté. C’est un paramètre important pour déterminer la force de l’infection plasmodiale. Elle est utilisée par les médecins pour affiner leur diagnostic et adapter leur traitement. Cette application est à destination des techniciens de laboratoire qui diagnostiquent le paludisme par microscopie (goutte épaisse et frottis sanguin).",
-        "help_user"               : "L’utilisateur fournit comme paramètres d'entrée le nombre de parasites et le nombre de globules blancs comptés par microscopie. Une valeur par défaut est utilisée pour le nombre de globules blancs par µL de sang. Cette valeur peut être modifiée. Le bouton 'Calculer' permet d'effectuer le calcul. Le bouton 'Réinitialiser' efface tous les champs."
+        "help_user"               : "L’utilisateur fournit comme paramètres d'entrée le nombre de parasites et le nombre de globules blancs comptés par microscopie. Une valeur par défaut est utilisée pour le nombre de globules blancs par µL de sang. Cette valeur peut être modifiée. Le bouton <em>Calculer</em> permet d'effectuer le calcul. Le bouton <em>Réinitialiser</em> efface tous les champs."
     },
     "en" : {
         "menu_home"               : "Home",
@@ -49,7 +49,7 @@ var translation = {
         "about_sponsors"          : "This project is supported by our sponsors:",
         "about_license"           : "This application is an open source software under the license",
         "help_description"        : "Parasite density is an estimation of the number of parasites per microliter of blood in an infected individual. It's an important factor to determine the strength of the plasmodial infection. It helps physicians to diagnose malaria and to adapt the treatment. This application is a tool for laboratory technicians who diagnose malaria by microscopy (thick or thin blood smear).",
-        "help_user"               : "The user provides as input parameters the number of parasites and the number of white blood cells measured by miscrocopy. A default value is used for the number of white blood cells per µL of blood. This value can be modified. The button 'Calculate' is used to perform the calculation of parasite density. The button 'Reset' is used to clean all the inputs and the obtained parasite density."
+        "help_user"               : "The user provides as input parameters the number of parasites and the number of white blood cells measured by miscrocopy. A default value is used for the number of white blood cells per µL of blood. This value can be modified. The button <em>Calculate</em> is used to perform the calculation of parasite density. The button <em>Reset</em> is used to clean all the inputs and the obtained parasite density."
     }
 }
 
